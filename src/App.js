3aac43318721bff308130d551be9648e6afe36ac
ahmedsubhani168@gmail.com <ahmedsubhani168@gmail.com>
2025-04-19 21:57:41 +0300

    added component files and seperated header and body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-800">
-      Hello Everyone. now we are building netflix-gpt
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
