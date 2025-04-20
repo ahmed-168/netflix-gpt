d1631d67d7f8e36a0414b170c996ba41665741b0
ahmedsubhani168@gmail.com <ahmedsubhani168@gmail.com>
2025-04-20 22:33:42 +0300

    added routes and img for header and body

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/69bec183-9cc8-49d4-8fc2-08228d3c91b4/web/QA-en-20250414-TRIFECTA-perspective_7418b76f-ef85-4a51-a04d-8953206c7459_large.jpg"
+        alt="netflix-bg"
+      ></img>
+    </div>
+  );
 };
 
 export default Login;
