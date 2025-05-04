68232562326862fc2f4ec2c1838a6bbbe998bc47
ahmedsubhani168@gmail.com <ahmedsubhani168@gmail.com>
2025-05-04 22:48:26 +0300

    validation impl

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
-import { checkValidData } from "../utils/validate";
+import { checkSignInValidData, checkSignUpValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  const handleButtonClick = () => {
-    // validate the form data
-    //checkValidData(email, password);
+  const email = useRef(null);
+  const password = useRef(null);
+  const fullName = useRef(null);
+
+  const handleButtonClick = (isSignInForm) => {
+    if (isSignInForm) {
+      const message = checkSignInValidData(
+        email.current.value,
+        password.current.value
+      );
+      setErrorMessage(message);
+    } else {
+      const message = checkSignUpValidData(
+        email.current.value,
+        password.current.value,
+        fullName.current.value
+      );
+      console.log(fullName);
+      setErrorMessage(message);
+    }
   };
 
   const toggleSignInForm = () => {
@@ -23,27 +41,34 @@ const Login = () => {
           alt="netflix-bg"
         ></img>
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto left-0 right-0 text-white rounded-lg bg-opacity-80">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto left-0 right-0 text-white rounded-lg bg-opacity-80"
+      >
         <h2 className="text-3xl font-bold py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h2>
         {!isSignInForm && (
           <input
+            ref={fullName}
             type="text"
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-gray-700"
           ></input>
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         ></input>
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         ></input>
+        <p className="text-red-700 font-bold py-2">{errorMessage}</p>
         <button
           className="p-4 my-6 bg-red-700 w-full rounded-lg"
           onClick={handleButtonClick}
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,4 @@
-export const checkValidData = (email, password) => {
+export const checkSignInValidData = (email, password) => {
   const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
     email
   );
@@ -9,7 +9,27 @@ export const checkValidData = (email, password) => {
 
   if (!isEmailValid) return "Email ID is not Valid";
 
-  if (!isPasswordValid) return "password is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+
+  return null;
+};
+
+export const checkSignUpValidData = (email, password, fullName) => {
+  const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(
+      password
+    );
+
+  const isFullNameValid = /^[A-Za-z][A-Za-z\s\-']{1,49}$/.test(fullName.trim());
+
+  if (!isEmailValid) return "Email ID is not Valid";
+
+  if (!isPasswordValid) return "Password is not valid";
+
+  if (!isFullNameValid) return "Full Name is not valid";
 
   return null;
 };
